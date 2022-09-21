a30102ac1f6ec56d61de68149317be634c0eabbd
Evan <81447921+EvanRothenbacher10@users.noreply.github.com>
2022-09-21 15:44:39 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    if x - 10 > y - 10:
+        return x
+    elif y - 10 > x - 10:
+        return y
+    else:
+        return 0
+
+print(close10(5, 13))
