51e07403d6499b4cefaa0d38fac1af67fb587008
Evan <81447921+EvanRothenbacher10@users.noreply.github.com>
2022-09-21 15:49:48 -0500

    2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def close10(x, y):
-    if x - 10 > y - 10:
-        return x
-    elif y - 10 > x - 10:
+    if abs(x - 10) > abs(y - 10):
         return y
+    elif abs(y - 10) > abs(x - 10):
+        return x
     else:
         return 0
 
